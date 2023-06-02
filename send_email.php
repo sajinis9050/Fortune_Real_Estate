5b0317d154a60efe71dbc53a383afbd1a9d75c70
Sajini Jeyaranjan <83372944+sajinis9050@users.noreply.github.com>
2023-06-01 23:07:59 -0400

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = 'test@email.com';
+    $headers = "From: $name <$email>";
+    $body = "Subject: $subject\n\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo 'Message sent successfully.';
+    } else {
+        echo 'An error occurred while sending the message.';
+    }
+}
+?>
